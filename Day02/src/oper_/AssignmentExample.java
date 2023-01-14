5643b0e8f3f78ed5320b06f6230d065232590f60
zooeylee <leemin00@hotmail.com>
2023-01-14 14:12:29 +0900

    이항연산자

---
+++ Day02/src/oper_/AssignmentExample.java
@@ -0,0 +1,13 @@
+package oper_;
+
+public class AssignmentExample {
+
+	public static void main(String[] args) {
+		//+=, -=, *=...등 = 앞에 다른 연산자를 작성 - 대입하면서 연산함
+		int a = 1;
+		a += 1; // a = a+1;
+		a =+ 1;
+		System.out.println(a);
+	}
+
+}
