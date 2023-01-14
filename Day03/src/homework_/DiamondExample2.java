b563b04397a5caf2bc52faf1933c18cb6f7b3e80
zooeylee <leemin00@hotmail.com>
2023-01-14 15:52:15 +0900

    중간에 $ 넣어보기  반복문 연습

---
+++ Day03/src/homework_/DiamondExample2.java
@@ -0,0 +1,52 @@
+package homework_;
+
+public class DiamondExample2 {
+
+	
+	
+	public static void main(String[] args) {
+		// 역 피라미드 만들기
+		for(int i=0; i<5; i++) { //0부터4까지 다섯번을 실행한다
+			for(int j=0; j<4-i; j++) { // 역으로 내려갈려면
+				System.out.print(" "); //i가0이면 4번실행, i가1이면 3번실행, i가 2이면 2번실행 공백을
+			}
+			
+			for(int j=0; j<i*2+1 ; j++) { // 1 3 5 7 9로 갈려면
+				if(j==0 || j==2*i) {
+					System.out.print("*");
+				} else {
+					if(j==0 || j==2*i) {
+						System.out.print(" ");	
+					} else {
+						System.out.print("$");
+					}
+				}	
+			}
+			System.out.println();
+		}
+	
+	
+	
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<i; j++) { // 역으로 내려갈려면
+				System.out.print(" ");
+			}
+		
+			for(int j=0; j<9-(i*2) ; j++) { // 1 3 5 7 9로 갈려면
+				if(j==0 || j==2*i) {
+					System.out.print("$");
+				} else {
+					System.out.println(" ");
+			}
+
+		
+		
+		System.out.println();
+	}
+	
+	
+}
+	
+
+
+
