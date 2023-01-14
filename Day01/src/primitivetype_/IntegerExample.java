b10f05583630a426e7d36e4cd6c06f77e43efac7
zooeylee <leemin00@hotmail.com>
2023-01-14 14:00:37 +0900

    integer 변수, 숫자를 표현하며 숫자에 대한 기본변수다. 지정없으면 int 변수로 숫자를 정의함

---
+++ Day01/src/primitivetype_/IntegerExample.java
@@ -0,0 +1,15 @@
+package primitivetype_;
+
+public class IntegerExample {
+
+	public static void main(String[] args) {
+		//숫자는 기본적으로 int형
+//		byte a = 128;
+//		short b = 32768;
+//		int c = 2192039102:
+		long d = 1L;
+		
+		
+	}
+
+}
