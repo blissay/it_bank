f84777c5d512da955df1e204900e249cea4b988e
zooeylee <leemin00@hotmail.com>
2023-01-15 17:47:07 +0900

    static의 사용예

---
+++ Day04/src/static_/MyConnection.java
@@ -0,0 +1,21 @@
+package static_;
+
+public class MyConnection {
+	// 서버가 터지지 않게 제한자 만 가지고 제한을 줄 수있다.
+	// 객체 제한을 할려면 생성자를 막아야 한다.
+	
+	private static MyConnection c = new MyConnection(); // c 리턴을 하기 위해 만들어줌
+	
+	private MyConnection() { // 생성자를 막음. 이건 내 클래스에서 밖에 못쓴다. 사용을 못하게 했으므로 막아두고 사용할 수 있는 방법을 메서드로 뚫여주자.
+		
+	}
+	
+	public static MyConnection getConnection() { // public메서드를 만드는데 꼭 static이 붙어야함. 
+	//	return new MyConnection():  // 이렇게 되면 제한이 없어서 안됨
+		return c; //private static MyConnection c = new MyConnection();이걸 만들고 c를 리턴한다
+	}
+	
+	
+	
+
+}
