221de83a99c62d6387cbbb3aa68ec63167b50949
zooeylee <leemin00@hotmail.com>
2023-01-15 15:22:00 +0900

    다이아몬드 안에 $표시하는것을 완성하는 과정이다

--- Day03/src/homework_/DiamondExample2.java
+++ Day03/src/homework_/DiamondExample2.java
@@ -2,20 +2,20 @@ package homework_;
 
 public class DiamondExample2 {
 
-	// 다이아몬드에서 중간에 *을 빼고 $모양을 넣는다. $모양은 j 값이 짝수일때 들어간다.
+	// 다이아몬드에서 중간에 공백으로 만든다
 	
 	public static void main(String[] args) {
 		//피라미드만들기
 		for(int i=0; i<5; i++) { //0부터4까지 다섯번을 실행한다
-			for(int j=0; j<4-i; j++) { // 역으로 내려갈려면
+			for(int j=0; j<4-i; j++) { // 역으로 내려갈려면 공백을 역으로 내려가면서 4,3,2,1
 				System.out.print(" "); //i가0이면 4번실행, i가1이면 3번실행, i가 2이면 2번실행 공백을
 			}
 			
-			for(int j=0; j<i*2+1 ; j++) { // 1 3 5 7 9로 갈려면
-				if(j==0 || j==2*i) {
+			for(int j=0; j<i*2+1 ; j++) { // 1 3 5 7 9로 갈려면 이렇게 하는데. *과 $를 엊갈려서 출력해야함.
+				if(j==0 || j==2*i) { // 첫번째 별나오고 2의 배수마다 출현해야하므로 또는 으로 두개를 함
 					System.out.print("*");
 				} else {
-					if(j==0 || j==2*i) {
+					if(j%2 == 1) { //2의배수일
 						System.out.print(" ");	
 					} else {
 						System.out.print("$");
@@ -27,25 +27,23 @@ public class DiamondExample2 {
 	
 	
 	
-		for(int i=0; i<5; i++) {
+		for(int i=0; i<4; i++) {
 			for(int j=0; j<i; j++) { // 역으로 내려갈려면
 				System.out.print(" ");
 			}
 		
-			for(int j=0; j<9-(i*2) ; j++) { // 1 3 5 7 9로 갈려면
+			for(int j=0; j<7-(i*2) ; j++) { // 1 3 5 7 9로 갈려면
 				if(j==0 || j==2*i) {
 					System.out.print("$");
 				} else {
 					System.out.println(" ");
+				}
 			}
-
-		
-		
-		System.out.println();
+			System.out.println();
+		}
+	}
 	
-	
-	
-
+}
 	
 
 
