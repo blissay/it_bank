78e1ac392b940036306289d02edee5c7546778a5
zooeylee <leemin00@hotmail.com>
2023-01-15 16:00:58 +0900

    사과를 먹는 사람으로 상속의 공부

---
+++ Day04/src/poly_/Person.java
@@ -0,0 +1,14 @@
+package poly_;
+
+public class Person {
+	//다형성 예제. 사람이 먹고 싶은 과일
+	//사람이 애플을 먹는다
+	
+	void eat(Apple apple) {
+		System.out.println("사람이 ")+apple.getName()+"를 먹습니다.";
+	}
+	
+	
+	
+
+}
