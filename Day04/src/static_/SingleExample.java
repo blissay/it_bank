0e0bce855a7d56a3a630ae99347d049599b622a5
zooeylee <leemin00@hotmail.com>
2023-01-15 18:50:01 +0900

    MyConnection을 참조해서

--- Day04/src/static_/SingleExample.java
+++ Day04/src/static_/SingleExample.java
@@ -9,14 +9,20 @@ public class SingleExample {
 	//	MyConnection c1 = new MyConnection(); // 이렇게 사용할 수없다.
 	//	MyConnection. 점을 찍어보면 메서드가 검색이 된다.
 		
-		MyConnection c1 = MyConnection.getConnection();
-		MyConnection c2 = MyConnection.getConnection();
+		MyConnection c1 = MyConnection.getConnection(); // MyConnectin 클래스에서 메서드를 불러서 사용할 수 있다.
+		MyConnection c2 = MyConnection.getConnection(); // private로 막은 클래스를 사용할 수 있는 방법. 은닉되어있음.
 		
 		System.out.println(c1); // 여러게가 만들어 졌는지 확인하는 코드
 		System.out.println(c2);
 		System.out.println(c1==c2); // 이름만 틀리고 같은거다
+		
+		
 
 
 	}
 
 }
+
+//결과 static_.MyConnection@1877ab81 // c1,c2가 같은 값과 주소를 가진다.
+//     static_.MyConnection@1877ab81
+//     true
