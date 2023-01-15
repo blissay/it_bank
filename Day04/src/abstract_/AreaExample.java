442919d0bd528b50457bdb59a4654f5d58219c06
zooeylee <leemin00@hotmail.com>
2023-01-15 18:44:21 +0900

    [abstract] 도형을 가지고 면적을 낸다.

---
+++ Day04/src/abstract_/AreaExample.java
@@ -0,0 +1,14 @@
+package abstract_;
+
+public class AreaExample {
+
+	public static void main(String[] args) {
+		// 추상클래스는 
+		
+		Shape shape = new Shape();
+		Shape circle = new circle();
+	
+
+	}
+
+}
