b0c1b7157d2c211d986510d9a40911527c080b5d
zooeylee <leemin00@hotmail.com>
2023-01-15 17:33:31 +0900

    static 변수에 대한 설명 추가

--- Day04/src/static_/Count.java
+++ Day04/src/static_/Count.java
@@ -4,7 +4,7 @@ public class Count {
 	
 	public int a; // 그냥선언
 	public static int b; //static이 붙여서 선언됨 
-	// 정적 , 고정된. 모든 instanc에 적용. 전부 공유한다.
+	// 정적 , 고정된. 모든 instanc에 적용. 전부 공유한다. 변수에 붙으면 다 공유된다.  Count.b  이렇게 호출한다.
 	
 	
 	
