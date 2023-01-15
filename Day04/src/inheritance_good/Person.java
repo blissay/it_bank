6276a6ef07a293343d712fc1e8a91522a8cef8a9
zooeylee <leemin00@hotmail.com>
2023-01-15 17:12:14 +0900

    static에 선언된곳에서 non-static은 사용할 수 없다. static이 선언된 클래스안에. this.age는 맞지 않는다.

--- Day04/src/inheritance_good/Person.java
+++ Day04/src/inheritance_good/Person.java
@@ -4,7 +4,7 @@ public class Person {
 
 	//서로다른  클래스에서 공통 중복을 따로 빼서 클래스를 만든다. 여러 클래스 안에 있는 중복을 피하는 방법이다.
 	//상속시켜주고싶은 코드를 적어놨다.
-	public String name;
+	public String name; //멤버 변수, 인스턴스 변수라고도 한다 객체별로 이 멤버가 가지고 있다.
 	public int age;
 	
 	public String getDetails() {
@@ -12,8 +12,14 @@ public class Person {
 		
 	}
 	
+	public static void setName(String name) { // static붙으면 this가 올수 없다.
+		this.name = name; // static에서 non-static애들은 쓸수 없다.
+	}
+	
+	
+	
 	public Person(String name, int age) {
-		this.name = name;
+		this.name = name; // this는 나자신이다. 정확히 말하면 person으로 만들어진 객체를 말한다. 내 만들어진 객체 내 인스턴스이 변수를 말한다.
 		this.age = age;
 	}
 	
