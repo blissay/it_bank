00e38344c57811c9fecb5112534e9609b7ed3e4e
zooeylee <leemin00@hotmail.com>
2023-01-15 15:23:49 +0900

    myDate클래스에서 변수를 public으로 막아놓고 myDate()를 만들어놓으니 그걸 불러오자

--- Day03/src/object_/MyDateExample.java
+++ Day03/src/object_/MyDateExample.java
@@ -3,9 +3,10 @@ package object_;
 public class MyDateExample {
 
 	public static void main(String[] args) {
-		//날짜를 출력할 수 있는 간단한 프로그램
+		//날짜를 출력할 수 있는 간단한 프로그램 , 직접 접근은 못하게
 		MyDate today = new MyDate();
 		today.year = 2023;
+		today.setYear(0);
 		today.month = 1;
 		today.day = 14;
 		today.getDate();
