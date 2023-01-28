a0082db87f0cf7063b0f6790b9745f6926c73ddd
zooeylee <leemin00@hotmail.com>
2023-01-28 17:46:33 +0900

    [에외] 멀티 엑셉션 예제

--- Day05/src/exception_/TriCatchExample.java
+++ Day05/src/exception_/TriCatchExample.java
@@ -6,14 +6,16 @@ import java.util.Scanner;
 public class TriCatchExample {
 
 	public static void main(String[] args) {
-		// 
+		// try에서 다양한 에외가 생길수 있다.
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("숫자 입력 : ");
 		int num = 0;
 		try {
 			num = scan.nextInt();  //에러가 될수 있는 부분이 여기다. 잘못된 값이 들어가면 예외 발생
-		}catch(InputMismatchException e) {  //RuntimeException어떤 에러가 뜰지 모르겟으면 적는다.
+			int ran = (int)Math.random()*2; // 0으로 나누어지는 에러 생긴다.
+			System.out.println(num / ran);
+		}catch(ArithmeticException | InputMismatchException e) {  //RuntimeException어떤 에러가 뜰지 모르겟으면 적는다. // 멀티 엑셉션
 			System.out.println("숫자만 입력하셔야 합니다.");
 			System.out.println("기본값 0 할당.");
 			
