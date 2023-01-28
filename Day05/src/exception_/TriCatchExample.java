3410ab2365bf721e35355848caf5db9e364b2861
zooeylee <leemin00@hotmail.com>
2023-01-28 17:41:01 +0900

    [예외] try catch구문 삽입으로 예외 해소

--- Day05/src/exception_/TriCatchExample.java
+++ Day05/src/exception_/TriCatchExample.java
@@ -1,5 +1,6 @@
 package exception_;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TriCatchExample {
@@ -9,8 +10,16 @@ public class TriCatchExample {
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("숫자 입력 : ");
-		int num = scan.nextInt();  //에러가 될수 있는 부분이 여기다. 잘못된 값이 들어가면 예외 발생
-		System.out.println("입력하신 숫자 : "+num);
+		int num = 0;
+		try {
+			num = scan.nextInt();  //에러가 될수 있는 부분이 여기다. 잘못된 값이 들어가면 예외 발생
+		}catch(InputMismatchException e) {  //RuntimeException어떤 에러가 뜰지 모르겟으면 적는다.
+			System.out.println("숫자만 입력하셔야 합니다.");
+			System.out.println("기본값 0 할당.");
+			
+		}
+		
+		System.out.println("입력하신 숫자 : "+num); //지역변수로 num을 사용할려면 앞에서 선언해 줘야한다.
 		scan.close();
 
 	}
