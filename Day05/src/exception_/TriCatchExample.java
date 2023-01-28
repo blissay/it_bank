f779cea56ab547c06dd74f311d3dd0d9862c2580
zooeylee <leemin00@hotmail.com>
2023-01-28 17:49:35 +0900

    [예외] 멀티 캐치를 이용한 예외. 캐치를 하나씩 해당 에외에 박아 넣는다.

--- Day05/src/exception_/TriCatchExample.java
+++ Day05/src/exception_/TriCatchExample.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class TriCatchExample {
 
 	public static void main(String[] args) {
-		// try에서 다양한 에외가 생길수 있다.
+		// try에서 다양한 에외가 생길수 있다. 
+		// catch를 하나 더 박는다. 멀치 캐치
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("숫자 입력 : ");
@@ -15,9 +16,12 @@ public class TriCatchExample {
 			num = scan.nextInt();  //에러가 될수 있는 부분이 여기다. 잘못된 값이 들어가면 예외 발생
 			int ran = (int)Math.random()*2; // 0으로 나누어지는 에러 생긴다.
 			System.out.println(num / ran);
-		}catch(ArithmeticException | InputMismatchException e) {  //RuntimeException어떤 에러가 뜰지 모르겟으면 적는다. // 멀티 엑셉션
+		}catch(InputMismatchException e) {  //RuntimeException어떤 에러가 뜰지 모르겟으면 적는다. // 멀티 엑셉션
 			System.out.println("숫자만 입력하셔야 합니다.");
 			System.out.println("기본값 0 할당.");
+			
+		}catch(ArithmeticException e){
+			System.out.println("ran이 0입니다.");
 			
 		}finally {
 			scan.close(); //길었을대 문제가 생길수 잇으므로 여기에 넣는다.
