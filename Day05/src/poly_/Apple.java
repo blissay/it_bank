c99392b0b1b269eb752c10eedcc409b74b2941e3
zooeylee <leemin00@hotmail.com>
2023-01-28 16:10:31 +0900

    [다중상속] fruit를 상속한 apple

---
+++ Day05/src/poly_/Apple.java
@@ -0,0 +1,15 @@
+package poly_;
+
+public class Apple extends Fruit{
+
+	private String name = "사과";
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String toString() {
+		return "사과";
+	}
+	
+}
