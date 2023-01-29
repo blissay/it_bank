97f28d9a34e6210b17d1891014ffa9679e5ded1f
zooeylee <leemin00@hotmail.com>
2023-01-29 15:23:28 +0900

    인터페이스로 명세서 역할을 한다. 주석문 추가

--- Day05/src/poly_/Fruit.java
+++ Day05/src/poly_/Fruit.java
@@ -4,6 +4,6 @@ public abstract class Fruit implements Food  {
 
 	
 	
-	public abstract String getName();
+	public abstract String getName(); //추상적인 메서드만 물려줘서 명세서 역할을 한다 인터페이스, 구현받고 물려받는데 여러개가 가능하다
 	
 }
