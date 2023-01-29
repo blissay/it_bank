70e974b77b9cbf193fca7fbab1b0f1ae11fe5cf7
zooeylee <leemin00@hotmail.com>
2023-01-29 14:50:54 +0900

    인터페이스 설명 추가

--- Day05/src/poly_/Person.java
+++ Day05/src/poly_/Person.java
@@ -5,7 +5,7 @@ public class Person {
 	void eat(Food food) {
 		System.out.println("사람이 "+ food.getName()+"를/을 먹습니다.");
 	}
-
+// 고기와 과일을 구분하지 않고 food  고기이자 음식, 과일이자 음식 으로 표현할 수 있었다.
 	
 	
 	public static void main(String[] args) {
