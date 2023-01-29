accda8d452a294276ba3ae1c8d006cba8cf11f59
zooeylee <leemin00@hotmail.com>
2023-01-29 16:16:55 +0900

    [tree계열] tree 구조에 대한 예제. 트리구조는 자동적으로 비교를 해서 정열을 한다. 참조형일때 비교가 안되므로 에외 발생. 그것을 해결하기 우해서는 comparable을 상속해서 사용하자.

---
+++ Day06/src/set_/TreeSetExample.java
@@ -0,0 +1,39 @@
+package set_;
+
+import java.util.*;
+
+public class TreeSetExample {
+
+	public static void main(String[] args) {
+		// 콜랙션에서 트리구조에 대한 예제
+		// 구조상. 크기비교해서 오름차순으로 자동 정열된다.
+		
+		Set<Integer> set = new TreeSet<>();
+		
+		set.add(10);set.add(1);set.add(5); // 다섯개 숫자만 넣어보자
+		set.add(6);set.add(4); 
+		
+		System.out.println(set); //[1,4,5,6,10]으로
+
+		Set<String> strSet = new TreeSet<>(); // 문자열은 순서가 있는지 궁금/
+		strSet.add("12");strSet.add("가나");strSet.add("나다");
+		strSet.add("ab");strSet.add("다");
+		
+		System.out.println(strSet); //[12, ab, 가나, 나다, 다] 이렇게 나옴. 아스키 코드 순서로 정열된다.첫글자로 크기 비교를 한다.
+
+		Set<Person> pset = new TreeSet<>();
+		pset.add(new Person("임영범",20));pset.add(new Person("박영준",20));
+		
+		System.out.println(pset); // classcastexception. 자료형이 못바꾸서 그렇다. person이 comparable로 못바꾼다. 연결이 안되어 있다.
+		// person클래스로 가서 인터페이스를 만들고 구현을 해서 크기 비교를 하자 implements Comparable<Person> 이렇게 comparable롤 implements 하면된다
+		
+//		public static Set<Integer> createLotto(){
+//			Set<Integer> lotto = new TreeSet<>();
+//			while(lotto.size() < 6) {  //6번까지만 한다
+//				lotto.add((int)(Math.random()*45+1)); //중복숫자는 안받으니까 중복없이 6개를 채운다.
+//			}
+//			return lotto;
+//		}
+	}
+
+}
