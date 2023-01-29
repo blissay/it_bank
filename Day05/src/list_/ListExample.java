14e867149708b198c0cedd5de438aa6cc463796e
zooeylee <leemin00@hotmail.com>
2023-01-29 15:22:58 +0900

    arraylist에 대한 설명추가

--- Day05/src/list_/ListExample.java
+++ Day05/src/list_/ListExample.java
@@ -7,7 +7,8 @@ public class ListExample {
 
 	public static void main(String[] args) {
 		// list 프레임 워크 사용 예제
-		// arraylist 속도가 빨라서 실제 데이타를 저장한다.
+		// arraylist 속도가 빨라서 실제 데이타를 저장한다. 입력 수정 삭제가 불편하다.번거롭다
+		//링크드 리스트는 노드가 나눠어 져서 노드끼리 주소로 연결되어있어서 입력 삭제가 쉽지만. 조회가 느리다
 	//	List list = new ArrayList(); // 어떤 데이타든지 다 들어갈수 있다. object다 스트링을 넣어도 오브젝트가 나온다. 한가지 타입만 들어간다면 제네릭을 사용 <>
 		List<String> list = new ArrayList<>();
 		list.add("123");list.add("456");list.add("78");
